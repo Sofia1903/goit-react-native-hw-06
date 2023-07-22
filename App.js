afdc79cb87d1f205fcb9a9557830b369a4b74f4c
Sofia <116076116+Sofia1903@users.noreply.github.com>
2023-07-22 16:15:49 +0300

    update

---
+++ App.js
@@ -0,0 +1,30 @@
+
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { Main } from './components/Main';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Medium': require('./assets/fonts/Roboto-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Main />
+        </PersistGate>
+      </Provider>
+    </>
+  );
+}
